7de1b9c81e0f4d6b20c475572f5e55ea5ef45d89
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2021-09-14 12:54:02 -0300

    Implementado controle de pontos de vida
    Implementado controle de pontos de vida e lógica responsável por tratar os erros.

--- js/jogo.js
+++ js/jogo.js
@@ -1,5 +1,6 @@
 var altura = 0
 var largura = 0
+var vidas = 1
 
 function ajustaTamanhoPalcoJogo() {
     altura = window.innerHeight
@@ -15,6 +16,14 @@ function posicaoRandomica() {
     //remover o mosquito anterior (caso exista)
     if (document.getElementById('mosquito')) {
         document.getElementById('mosquito').remove()
+
+        if (vidas > 3) {
+            alert('Vidas Esgotadas (Game Over)')
+        } else {
+            document.getElementById('v' + vidas).src = 'img/coracao_vazio.png'
+            vidas++
+        }
+
     }
 
 
@@ -36,6 +45,9 @@ function posicaoRandomica() {
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
     mosquito.id = 'mosquito'
+    mosquito.onclick = function() {
+        this.remove()
+    }
 
     document.body.appendChild(mosquito)
 }
