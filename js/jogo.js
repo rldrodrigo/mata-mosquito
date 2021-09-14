5521e985e250066290062400f50c90628e9ffd3b
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2021-09-14 10:49:11 -0300

    Criação da Função posicaoRandomica
    Foi implementado a imagem do mosquito e a lógica de posição randômica dele

--- js/jogo.js
+++ js/jogo.js
@@ -5,7 +5,29 @@ function ajustaTamanhoPalcoJogo() {
     altura = window.innerHeight
     largura = window.innerWidth
 
-    console.log(altura, largura)
+    //console.log(altura, largura)
 }
 
 ajustaTamanhoPalcoJogo()
+
+function posicaoRandomica() {
+    var posicaoX = Math.floor(Math.random() * largura) - 90
+    var posicaoY = Math.floor(Math.random() * altura) - 90
+
+    //Utiliza operador Ternário para não receber posição negativa
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+    console.log(altura, largura)
+    console.log(posicaoX, posicaoY)
+
+    //Criar o elemento HTML
+    var mosquito = document.createElement('img')
+    mosquito.src = 'img/mosca.png'
+    mosquito.className = 'mosquito1'
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+
+    document.body.appendChild(mosquito)
+}
